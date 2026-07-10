45db49c2d2a5202532939fe01f45135142f5562b
zyt <me@dreams.plus>
2026-07-10 17:47:22 +0800

    fix: remove unused files.

--- examples/atlas_paper_fig_4_demo_multi_file_example/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "math_utils.h"
-
-int main(void) {
-    int f = factorial(5);
-    int last = get_last_result();
-    printf("fact=%d last=%d\n", f, last);
-    return 0;
-}
--- examples/multi/utils.h
+++
@@ -1,8 +0,0 @@
-// Header file with function declarations
-#ifndef UTILS_H
-#define UTILS_H
-
-int add(int x, int y);
-int subtract(int x, int y);
-
-#endif
